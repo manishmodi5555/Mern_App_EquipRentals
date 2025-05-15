12c8fd44439a62381cb8f51b2fda142ba61c29e6
manishmodi555 <manishmodi314@gmail.com>
2025-05-15 18:14:45 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -87,11 +87,7 @@ app.use(
 );
 
 app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003"],
-    credentials: true,  
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
+  cors()
 );
 
 connectDB();
